336a77ad83224606c6dba466c3f7235cfb0bb993
Kareem Missoumi <kmissoumi@gmail.com>
2023-07-25 15:07:47 +0200

    Updates and feedback following alpha pass.

--- usr/get-info.sh
+++ usr/get-info.sh
@@ -1,10 +1,21 @@
 #!/bin/sh -x
 
-pwd             > pwd.log   2>&1
-env             > env.log   2>&1
-ls -la          > ls.log    2>&1
-java -version   > java.log  2>&1
-npm --version   > npm.log   2>&1
-tree            > tree1.log 2>&1
-tree /usr/build > tree2.log 2>&1
-tree /root      > tree3.log 2>&1
+logPath="/var/log"
+
+pwd             > ${logPath}/pwd.log    2>&1
+env             > ${logPath}/env.log    2>&1
+getconf -a      > ${logPath}/gconf.log  2>&1
+ls -la          > ${logPath}/ls.log     2>&1
+java -version   > ${logPath}/java.log   2>&1
+npm --version   > ${logPath}/npm.log    2>&1
+node --version  > ${logPath}/node.log   2>&1
+
+tree /          > ${logPath}/tree_slash.log 2>&1
+tree /usr       > ${logPath}/tree_usr.log   2>&1
+tree /usr/build > ${logPath}/tree_build.log 2>&1
+tree /root      > ${logPath}/tree_root.log  2>&1
+tree /var/log   > ${logPath}/tree_var.log   2>&1
+
+which sh bash node npm tree pwd env ls > which.log 2>&1
+
+exit 0
