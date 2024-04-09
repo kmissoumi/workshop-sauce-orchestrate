ef0a7a94242f59ab6241f0637d8dc5dae718d264
Kareem Missoumi <kmissoumi@gmail.com>
2024-04-10 00:40:42 +0200

    Now with better error handling.

--- usr/wiremock-runner.sh
+++ usr/wiremock-runner.sh
@@ -15,31 +15,40 @@ install_sauce_connect() {
 
 
 check_wiremocks() {
+    wireMockRc=0
     i=${WIREMOCK_PORT_START}
     until [ $i -gt ${WIREMOCK_PORT_END} ]
     do
         check_wiremock ${i}
+        ((wireMockRc=wireMockRc+$?))
         ((i=i+1))
     done
+    export wireMockRc
 }
 
 
 check_wiremock() {
-    set -e 
     wireMockPort=${1}
-    t=5
-    printf '\n Waiting for WireMock-%s to start...\n' ${wireMockPort}
-    wget --retry-connrefused --retry-on-http-error=400,503 --tries=${t} http://test-runner.internal:${wireMockPort}/__admin/mappings \
-        && printf 'WireMock-%s is ready!\n' ${wireMockPort} || printf 'WireMock-%s is NOTOK!\n' ${wireMockPort}
+    hostName="test-runner.internal"
+    tries=2
+    printf '\nWaiting for WireMock-%s to start...\n' ${wireMockPort}
+    wget --retry-connrefused --retry-on-http-error=400,503 --tries=${tries} http://${hostName}:${wireMockPort}/__admin/mappings?limit=1
+    wgetRc=$?
+    [[ ${wgetRc} = 0 ]] && printf 'WireMock-%s is ready!\n' ${wireMockPort} || printf 'WireMock-%s is NOTOK!\n' ${wireMockPort}
+    return ${wgetRc}
 }
 
 
 main(){
-    set -e
     sleep 5
     install_utilities
     install_sauce_connect
     check_wiremocks
+    if [[ ${wireMockRc} -ge 1 ]]; then
+        printf '\nWireMock Services NOTOK...exiting\n'
+        return 4
+    fi
+    printf '\nWireMock Services are OK...starting Sauce Connect!\n'
     export SAUCE_METADATA="runner=orchestrate,runID=${__SO_UUID}"
     /usr/bin/sc run
 }
